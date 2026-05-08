135d0ef591ce373e67e232a554dd11db9272092d
IIIWA <iiiwaaa6@gmail.com>
2026-05-08 04:17:11 +0300

    Work with html optimization

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -10,5 +14,31 @@ export default defineConfig({
     build: {
         outDir: '../dist',
         emptyOutDir: true,
+        assetsInlineLimit: 0,
+        rollupOptions: {
+            input: {
+                main: './src/index.html',
+                about: './src/about.html',
+            },
+        },
     },
+    plugins: [
+        injectHTML(),
+        ViteMinifyPlugin(),
+        ViteImageOptimizer({
+            /* pass your config */
+            png: {
+                quality: 70,
+            },
+            jpeg: {
+                quality: 70,
+            },
+            jpg: {
+                quality: 70,
+            },
+            webp: {
+                quality: 70,
+            },
+        }),
+    ],
 });
